a4379c14a0a878eeaab36b271b8483c90fa7c8ff
Lukasz Macon <macon.lukasz@wp.pl>
2020-02-11 18:48:33 +0100

    quiz form

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  db:
-    "mongodb+srv://admin:6jBiiZ86TLOBNKWk@cluster0-kqpx4.mongodb.net/test?retryWrites=true&w=majority",
+  db: "",
   keySession: ["TWOJKLUCZ"],
   maxAgeSession: 24 * 60 * 60 * 1000
 };
