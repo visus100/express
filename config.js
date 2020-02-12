966246299fed36c754f3d586047cdddacdf7f4db
Lukasz Macon <macon.lukasz@wp.pl>
2020-02-12 01:21:07 +0100

    config db

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  db: "",
+  db:
+    "mongodb+srv://admin:6jBiiZ86TLOBNKWk@cluster0-kqpx4.mongodb.net/test?retryWrites=true&w=majority",
   keySession: ["TWOJKLUCZ"],
   maxAgeSession: 24 * 60 * 60 * 1000
 };
